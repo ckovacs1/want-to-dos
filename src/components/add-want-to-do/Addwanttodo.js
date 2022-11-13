f2ea5c103ec9f354e1f13cddeba9c5090b66099b
les5185 <70672721+les5185@users.noreply.github.com>
2022-11-13 12:09:11 -0500

    Finish up add want to do

--- src/components/add-want-to-do/Addwanttodo.js
+++ src/components/add-want-to-do/Addwanttodo.js
@@ -1,4 +1,8 @@
+import { SettingsBluetoothSharp } from '@mui/icons-material';
 import {
+  Box,
+  Button,
+  Chip,
   FormControl,
   InputLabel,
   MenuItem,
@@ -10,6 +14,9 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import dayjs from 'dayjs';
 import React from 'react';
 import { useState } from 'react';
+import './Addwanttodo.css';
+
+const DAYS = ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat'];
 
 function Addwanttodo() {
   const [inputs, setInputs] = useState({
@@ -18,9 +25,9 @@ function Addwanttodo() {
     startDateTime: dayjs('2022-11-01'),
     endDateTime: dayjs('2022-11-01'),
     repetition: '', //select
-    repeatOn: [], //select multiselect
+    repeatOn: [], //multiselect
     category: '', //select
-    inviteFriends: [], //multiselect
+    inviteFriends: [], //multi input and delete
   });
 
   const {
@@ -34,6 +41,30 @@ function Addwanttodo() {
     inviteFriends,
   } = inputs;
 
+  //usestate for invitefriends
+  const [inviteFriendsInput, setInviteFriendsInput] = useState('');
+
+  const onChangeInviteFriendsInput = e => {
+    setInviteFriendsInput(e.target.value);
+  };
+
+  const onKeydownInviteFriendsInput = e => {
+    if (e.key === 'Enter') {
+      setInputs({
+        ...inputs,
+        inviteFriends: [...inviteFriends, inviteFriendsInput],
+      });
+      setInviteFriendsInput('');
+    }
+  };
+
+  const onDeleteInviteFriend = target => {
+    setInputs({
+      ...inputs,
+      inviteFriends: inviteFriends.filter(friend => friend !== target),
+    });
+  };
+
   const onChange = e => {
     const { name, value } = e.target;
     setInputs({
@@ -42,79 +73,183 @@ function Addwanttodo() {
     });
   };
 
+  const onClickRepeatOnButton = day => {
+    if (repeatOn.includes(day)) {
+      //delete from array
+      setInputs({
+        ...inputs,
+        repeatOn: repeatOn.filter(d => d !== day),
+      });
+    } else {
+      //add to array
+      setInputs({
+        ...inputs,
+        repeatOn: [...repeatOn, day],
+      });
+    }
+  };
+
   return (
-    <div>
-      <TextField
-        type="text"
-        name="title"
-        label="Title"
-        onChange={onChange}
-        value={title}
-      />
-      <TextField
-        type="text"
-        name="description"
-        label="Description"
-        onChange={onChange}
-        value={description}
-      />
-      <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <DateTimePicker
-          renderInput={props => <TextField {...props} />}
-          label="startDateTime"
-          value={startDateTime}
-          onChange={newValue => {
-            setInputs({
-              ...inputs,
-              startDateTime: newValue,
-            });
-          }}
-        />
-      </LocalizationProvider>
-      <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <DateTimePicker
-          renderInput={props => <TextField {...props} />}
-          label="endDateTime"
-          value={endDateTime}
-          onChange={newValue => {
-            setInputs({
-              ...inputs,
-              endDateTime: newValue,
-            });
-          }}
-        />
-      </LocalizationProvider>
-      <FormControl sx={{ width: '150px' }}>
-        <InputLabel id="repetition-select">Repetition</InputLabel>
-        <Select
-          labelId="repetition-select"
-          value={repetition}
-          label="Repetition"
-          name="repetition"
-          onChange={onChange}
-        >
-          <MenuItem value="weekly">Weekly</MenuItem>
-          <MenuItem value="monthly">Monthly</MenuItem>
-          <MenuItem value="yearly">Yearly</MenuItem>
-        </Select>
-      </FormControl>
-      <FormControl sx={{ width: '150px' }}>
-        <InputLabel id="category-select">Category</InputLabel>
-        <Select
-          labelId="category-select"
-          value={category}
-          label="Category"
-          name="category"
-          onChange={onChange}
-        >
-          <MenuItem value="fitness">Fitness</MenuItem>
-          <MenuItem value="nutrition/diet">Nutrition/Diet</MenuItem>
-          <MenuItem value="lifestyle">Lifestyle</MenuItem>
-          <MenuItem value="skills/learning">Skills/Learning</MenuItem>
-          <MenuItem value="mindfulness">Mindfulness</MenuItem>
-        </Select>
-      </FormControl>
-    </div>
+    <Box
+      sx={{
+        width: '100vw',
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <div className="addwanttodo__wrapper">
+        <div className="addwanttodo__title">
+          <label>Title</label>
+          <TextField
+            type="text"
+            name="title"
+            label="Title"
+            onChange={onChange}
+            value={title}
+            className="addwanttodo__title-input"
+          />
+        </div>
+        <div className="addwanttodo__description">
+          <label>Description</label>
+          <TextField
+            type="text"
+            name="description"
+            label="Description"
+            onChange={onChange}
+            value={description}
+            className="addwanttodo__description-input"
+          />
+        </div>
+        <div className="addwanttodo__date">
+          <div className="addwanttodo__date-start">
+            <label className="addwanttodo__date-start-label">Starts on</label>
+            <LocalizationProvider
+              dateAdapter={AdapterDayjs}
+              className="addwanttodo__date-start-input"
+            >
+              <DateTimePicker
+                renderInput={props => <TextField {...props} />}
+                label="startDateTime"
+                value={startDateTime}
+                onChange={newValue => {
+                  setInputs({
+                    ...inputs,
+                    startDateTime: newValue,
+                  });
+                }}
+              />
+            </LocalizationProvider>
+          </div>
+          <div className="addwanttodo__date-end">
+            <label className="addwanttodo__date-end-label">Ends on</label>
+            <LocalizationProvider
+              dateAdapter={AdapterDayjs}
+              className="addwanttodo__date-end-input"
+            >
+              <DateTimePicker
+                renderInput={props => <TextField {...props} />}
+                label="endDateTime"
+                value={endDateTime}
+                onChange={newValue => {
+                  setInputs({
+                    ...inputs,
+                    endDateTime: newValue,
+                  });
+                }}
+              />
+            </LocalizationProvider>
+          </div>
+        </div>
+
+        <div className="addwanttodo__repetition">
+          <FormControl sx={{ width: '150px' }}>
+            <InputLabel id="repetition-select">Repetition</InputLabel>
+            <Select
+              labelId="repetition-select"
+              value={repetition}
+              label="Repetition"
+              name="repetition"
+              onChange={onChange}
+            >
+              <MenuItem value="weekly">Weekly</MenuItem>
+              <MenuItem value="monthly">Monthly</MenuItem>
+              <MenuItem value="yearly">Yearly</MenuItem>
+            </Select>
+          </FormControl>
+        </div>
+
+        <div className="addwanttodo__category">
+          <FormControl sx={{ width: '150px' }}>
+            <InputLabel id="category-select">Category</InputLabel>
+            <Select
+              labelId="category-select"
+              value={category}
+              label="Category"
+              name="category"
+              onChange={onChange}
+            >
+              <MenuItem value="fitness">Fitness</MenuItem>
+              <MenuItem value="nutrition/diet">Nutrition/Diet</MenuItem>
+              <MenuItem value="lifestyle">Lifestyle</MenuItem>
+              <MenuItem value="skills/learning">Skills/Learning</MenuItem>
+              <MenuItem value="mindfulness">Mindfulness</MenuItem>
+            </Select>
+          </FormControl>
+        </div>
+
+        <div className="addwanttodo__invite-friends">
+          <TextField
+            type="text"
+            label="Invite Friends"
+            onChange={onChangeInviteFriendsInput}
+            onKeyDown={onKeydownInviteFriendsInput}
+            value={inviteFriendsInput}
+          />
+          {inviteFriends.map(friend => (
+            <Chip
+              key={friend}
+              label={friend}
+              variant="outlined"
+              onDelete={() => onDeleteInviteFriend(friend)}
+            />
+            //add validate id later
+          ))}
+        </div>
+        <div className="addwanttodo__repeaton">
+          <label>Repeat on</label>
+          <div className="addwanttodo__repeaton-buttons">
+            {DAYS.map(day => (
+              <Button
+                variant={repeatOn.includes(day) ? 'contained' : 'outlined'}
+                sx={{ mx: 0.5 }}
+                onClick={() => onClickRepeatOnButton(day)}
+                key={day}
+              >
+                {day}
+              </Button>
+            ))}
+          </div>
+        </div>
+        <div className="addwanttodo__submit">
+          <Button
+            variant="contained"
+            onClick={() => {}}
+          >
+            {' '}
+            Add{' '}
+          </Button>
+          <Button
+            variant="outlined"
+            onClick={() => {}}
+          >
+            {' '}
+            Cancel
+          </Button>
+        </div>
+      </div>
+    </Box>
   );
 }
 
