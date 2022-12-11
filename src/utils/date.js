814943aa19e5a241bce6df7527bf7629c3f6b2f0
les5185 <70672721+les5185@users.noreply.github.com>
2022-12-10 22:35:58 -0500

    add repeat logic

--- src/utils/date.js
+++ src/utils/date.js
@@ -8,3 +8,15 @@ export const checkToday = datetime => {
   const todayDate = new Date();
   return targetDate.setHours(0, 0, 0, 0) === todayDate.setHours(0, 0, 0, 0);
 };
+
+/**
+ * Check datetime is tomorrow
+ * @param {string} datetime
+ * @return {boolean}
+ */
+export const checkTomorrow = datetime => {
+  const targetDate = new Date(datetime);
+  const tomorrowDate = new Date();
+  tomorrowDate.setDate(tomorrowDate.getDate() + 1);
+  return targetDate.setHours(0, 0, 0, 0) === tomorrowDate.setHours(0, 0, 0, 0);
+};
