31d298f2c670299b8081c814fdc400ba7d1d5fa4
les5185 <70672721+les5185@users.noreply.github.com>
2022-12-15 20:41:07 -0500

    fix follow notif

--- src/api/request.js
+++ src/api/request.js
@@ -1,6 +1,6 @@
 const DEV_HOST =
   process.env.NODE_ENV === 'production'
     ? 'https://todo-backend-bk5n.onrender.com'
-    : 'http://localhost:5002';
+    : 'http://localhost:5000';
 
 export const getRequestUrl = path => `${DEV_HOST}${path}`;
